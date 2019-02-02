56809d453976297f5870e7ca9fb73181d497d29d
Mike King <michael.y.king@nab.com.au>
2019-02-02 12:33:29 +1100

    Improving tests

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 
 const chalk = require('chalk')
 const fs = require('fs')
+const debug = require('debug')('askmike')
 
 const getip = require('./lib/ip')
 const nabCheck = require('./lib/nab-check')
@@ -10,6 +11,7 @@ var opts = require('minimist')(process.argv.slice(2))
 // console.dir(opts)
 
 const target = opts._[0]
+let errors = []
 
 switch (target) {
   case 'ip':
@@ -17,10 +19,25 @@ switch (target) {
     console.log(`IP Addresses: \n${addresses.join('\n')}`)
     break
   case 'nab-check':
-    nabCheck(opts)
+    errors = nabCheck(opts)
     // No output - command will do its own
     break
   default:
     console.log("Please provide a command as a parameter, eg 'ip' or 'nab-check'")
     break
+}
+
+// debug('errors', errors)
+try {
+  if (errors.length > 0) {
+    // console.log(errors.join('\n'))
+    debug('FAIL')
+    process.exit(1)
+  } else {
+    debug('OK')
+    debug(`${target}: ${chalk.green.inverse('ok')}`)
+    process.exit(0)
+  }
+} catch (e) {
+  console.error(e)
 }
--- cli.test.js
+++ cli.test.js
@@ -1,85 +1,16 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const mkpath = require("mkpath");
-const { execSync } = require("child_process");
+const fs = require('fs')
+const { execSync } = require('child_process')
 
-// base is defined as a location outside a git repo. Assumes I have write
-// access to .. :)
-const base = "../.kebab-ify-test";
-const widgetContents = `
-//# 
-//# Test file
-//#
-import Avatar from '../../Components/Avatar'
-import SomeThing from '../../Components/SomeThing'
-import SomeThingElse from '../../components/SomeThingElse'
-//
-// The rest of the file isn't important
-//
-`;
-const avatarContents = `
-//# 
-//# Avatar component test file
-//#
-import SomeThing from '../../components/SomeThing'
-//
-// The rest of the file isn't important
-//
-`;
+test('Prefix ok', () => {
+  expect(() => {
+    execSync('node cli.js nab-check')
+  }).toThrow()
+})
 
-afterAll(() => {
-  execSync(`rm -rf ${base}`);
-});
-
-//
-// Defensive cleanup of old test folder
-//
-if (fs.existsSync(base)) {
-  execSync(`rm -rf ${base}/*`);
-}
-mkpath.sync(`${base}/src/Components/MyWidget`);
-const widgetsrc = `${base}/src/Components/MyWidget/MyWidget.js`;
-fs.writeFileSync(widgetsrc, widgetContents, {
-  encoding: "utf8"
-});
-const avatarsrc = `${base}/src/Components/Avatar.js`;
-fs.writeFileSync(avatarsrc, avatarContents, {
-  encoding: "utf8"
-});
-const setupTestsFile = `${base}/src/setupTests.ts`;
-fs.writeFileSync(setupTestsFile, "Contents not important", {
-  encoding: "utf8"
-});
-
-test("Files are set up", async () => {
-  await expect(fs.existsSync(base)).toBeTruthy();
-  await expect(fs.existsSync(widgetsrc)).toBeTruthy();
-});
-
-test("Kebab-ify!!!", () => {
-  expect(execSync("kebab-ify src", { cwd: base })).toBeTruthy();
-});
-
-test("PascalCase files are not there any more", () => {
-  expect(fs.existsSync(base)).toBeTruthy();
-  // Because MacOS isn't case sensitive, this folder appears to be there, even though it
-  // has been renamed, so we can't do this test
-  // expect(fs.existsSync(`${base}/src/Components`)).toBeFalsy()
-  expect(fs.existsSync(`${base}/src/Components/MyWidget`)).toBeFalsy();
-  expect(
-    fs.existsSync(`${base}/src/Components/MyWidget/MyWidget.js`)
-  ).toBeFalsy();
-});
-
-test("kebab-case files are there", () => {
-  expect(fs.existsSync(base)).toBeTruthy();
-  expect(fs.existsSync(`${base}/src/components`)).toBeTruthy();
-  expect(
-    fs.existsSync(`${base}/src/components/my-widget/my-widget.js`)
-  ).toBeTruthy();
-});
-
-test("setupTests file is preserved", async () => {
-  await expect(fs.existsSync(setupTestsFile)).toBeTruthy();
-});
+test('Prefix ', () => {
+  expect(() => {
+    execSync('NPM_prefix=BAD_THING node cli.js nab-check')
+  }).toThrow()
+})
